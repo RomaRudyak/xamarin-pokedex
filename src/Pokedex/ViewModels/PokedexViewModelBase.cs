80f10d13ab6945c4d4ba14c46b090dbeb6f4b0cd
Roma.Rudyak <roma.rudyak@gmail.com>
2017-01-23 20:22:14 +0200

    Core Project

---
+++ src/Pokedex/ViewModels/PokedexViewModelBase.cs
@@ -0,0 +1,13 @@
+﻿using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokedex.ViewModels
+{
+    public class PokedexViewModelBase : BaseViewModel
+    {
+    }
+}
